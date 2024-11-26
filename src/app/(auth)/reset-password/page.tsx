b1f4029bdc2fb6f32e88e7206900be95540c1bc7
Diljan <diljan@Diljans-MacBook-Air.local>
2024-11-27 04:10:54 +0530

    import  fix

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,6 +1,7 @@
+"use client";
+
 export const dynamic = 'force-dynamic';
 
-"use client";
 import React, { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { resetPasswordTokenCheckAction } from "./actions";
